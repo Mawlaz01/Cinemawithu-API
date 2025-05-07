bb4ee6d4c7f73ead8767e6c3e1b4dd528e7f293b
Mawlaz01 <agielsingo@gmail.com>
2025-05-07 18:03:48 +0700

    fix: error

--- model/adminModel.js
+++ model/adminModel.js
@@ -12,18 +12,6 @@ class AdminModel {
             })
         })
     }
-
-    static async login(email) {
-        return new Promise((resolve, reject) => {
-            connection.query('SELECT * FROM admins WHERE email = ?', [email], (err, rows) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(rows[0])
-                }
-            })
-        })
-    }    
 }
 
 module.exports = AdminModel
