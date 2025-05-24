cba4f638d400c6f5be4a22cd28e5a293ae85711d
Mawlaz01 <agielsingo@gmail.com>
2025-05-24 20:48:15 +0700

    feat: user detail film

--- model/showtimeModel.js
+++ model/showtimeModel.js
@@ -8,7 +8,7 @@ class showtimeModel {
                 FROM showtimes s
                 JOIN films f ON s.film_id = f.film_id
                 JOIN theaters t ON s.theater_id = t.theater_id
-                ORDER BY s.start_time
+                ORDER BY s.date, s.time
             `, (err, results) => {
                 if (err) {
                     reject(err)
@@ -39,14 +39,14 @@ class showtimeModel {
 
     static async getByFilmId(filmId) {
         return new Promise((resolve, reject) => {
-            connection.query(`
-                SELECT s.*, f.title AS film_title, t.name AS theater_name 
+            const query = `
+                SELECT s.*, t.name as theater_name, t.total_seats
                 FROM showtimes s
-                JOIN films f ON s.film_id = f.film_id
                 JOIN theaters t ON s.theater_id = t.theater_id
                 WHERE s.film_id = ?
-                ORDER BY s.start_time
-            `, [filmId], (err, results) => {
+                ORDER BY s.date ASC, s.time ASC
+            `
+            connection.query(query, [filmId], (err, results) => {
                 if (err) {
                     reject(err)
                 } else {
@@ -64,7 +64,7 @@ class showtimeModel {
                 JOIN films f ON s.film_id = f.film_id
                 JOIN theaters t ON s.theater_id = t.theater_id
                 WHERE s.theater_id = ?
-                ORDER BY s.start_time
+                ORDER BY s.date, s.time
             `, [theaterId], (err, results) => {
                 if (err) {
                     reject(err)
@@ -77,11 +77,11 @@ class showtimeModel {
 
     static async create(data) {
         return new Promise((resolve, reject) => {
-            const { film_id, theater_id, start_time, price } = data
+            const { film_id, theater_id, date, time, price } = data
             
             connection.query(
-                'INSERT INTO showtimes (film_id, theater_id, start_time, price) VALUES (?, ?, ?, ?)',
-                [film_id, theater_id, start_time, price],
+                'INSERT INTO showtimes (film_id, theater_id, date, time, price) VALUES (?, ?, ?, ?, ?)',
+                [film_id, theater_id, date, time, price],
                 (err, results) => {
                     if (err) {
                         reject(err)
@@ -95,11 +95,11 @@ class showtimeModel {
 
     static async update(id, data) {
         return new Promise((resolve, reject) => {
-            const { film_id, theater_id, start_time, price } = data
+            const { film_id, theater_id, date, time, price } = data
             
             connection.query(
-                'UPDATE showtimes SET film_id = ?, theater_id = ?, start_time = ?, price = ? WHERE showtime_id = ?',
-                [film_id, theater_id, start_time, price, id],
+                'UPDATE showtimes SET film_id = ?, theater_id = ?, date = ?, time = ?, price = ? WHERE showtime_id = ?',
+                [film_id, theater_id, date, time, price, id],
                 (err, results) => {
                     if (err) {
                         reject(err)
